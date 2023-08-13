22400998434d8b0a7bfe37ebeed6fe18e0375272
NIDAMUKESH <nidamukesh@gmail.com>
2023-08-13 09:35:00 +0500

    screens comp

--- user.js
+++ user.js
@@ -1,15 +1,41 @@
 let searchform = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    Order.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let shoppingCart= document.querySelector('.shopping-cart');
 document.querySelector('#shoppingcart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
+    searchform.classList.remove('active');
+    Order.classList.remove('active');
+    navbar.classList.remove('active');
+    
 }
+
 let Order= document.querySelector('.OrderForm');
 document.querySelector('#login-btn').onclick = () =>{
     Order.classList.toggle('active');
+    searchform.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar= document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    Order.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchform.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    Order.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 const boxes = document.querySelectorAll('.box');
